3dc05765b77ac36c48a29f299c40f17c235f8807
shmin <gks831@naver.com>
2022-08-02 19:21:40 +0900

    [DISC-1] 프로젝트 최초 생성 및 Header 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import menu_icon from './img/menu.png';
+import logo_text from './img/logo_text2.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className='navbar'>
+				<img src={logo_text} className='logo_text' alt='personality_behaviors'></img>
+				<img src={menu_icon} className='menu_icon' alt='menu_icon'></img>
+			</div>
+		</div>
+	);
 }
 
 export default App;
