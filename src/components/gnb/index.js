6aac7c7888d2e5c385ad1dc307be6c531d1d2e49
shmin <gks831@naver.com>
2022-08-03 16:27:22 +0900

    [DISC-3] GNB 메뉴 개발 2

--- src/components/gnb/index.js
+++ src/components/gnb/index.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import exit from '../../img/exit.png';
 import styles from './index.module.css';
 
 function Gnb(props) {
-
     return (
         <>
         <div className={props.isGnb ? styles.gnb_menu_active : styles.gnb_menu_hidden}>
-            <div className={styles.gnb_background} onClick={props.fnIsGnb}>
-
-            </div>
+            <div className={styles.gnb_background}></div>
             <div className={styles.gnb_body}>
                 <div className={styles.gnb_body_header}>
-                    <div className={styles.gnb_body_close}>X</div>
+                    <img src={exit} className={styles.gnb_exit} onClick={props.fnIsGnb} alt='exit_button'></img>
                 </div>
+                <div className={styles.gnb_wrap}>
+                    <li className={styles.gnb_frst}>검사하기</li>
+                    <li className={styles.gnb_list}>DISC 란</li>
+                </div>
+                <div className={styles.gnb_footer}>8ugust</div>
             </div>
         </div>
         </>
