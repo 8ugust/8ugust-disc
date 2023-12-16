04b228b609d31b1c8428a311008239f56f78ea66
shmin <gks831@naver.com>
2023-12-16 14:13:13 +0900

    Create SVG Tag

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+	fnSetSvgWidth();
+});
+
+
+const fnSetSvgWidth = () => {
+	const per_30 = Math.round(Number(window.innerWidth) * 0.35);
+	const per_60 = Math.round(Number(window.innerWidth) * 0.7);
+
+	let dPath = "";
+	dPath += "M0 0 L0 60 ";
+	dPath += "L" + per_30 + " 150 ";
+	dPath += "L" + per_60 + " 20 ";
+	dPath += "L" + window.innerWidth + " 100 ";
+	dPath += "L" + window.innerWidth + " 0 ";
+
+	// M0 0 L0 60 L350 150 L700 20 L980 100 L980 0
+	const svg_desc = document.getElementById('svg-desc');
+	svg_desc.setAttribute("d", dPath);
+
+	console.log(svg_desc.attributes.d);
+}
