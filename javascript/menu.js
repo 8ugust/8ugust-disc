850253c5461f3ad58a35aa7b9e85075c1b0ac0ae
shmin <gks831@naver.com>
2024-01-04 00:07:02 +0900

    Add Menu

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,12 +1,23 @@
 const fnMenuClick = () => {
+	let img = document.getElementsByClassName('header-wrap')[0].children[2];
 	const menu = document.getElementById('menu-wrap');
 	if (menu.style.display == 'none') {
 		menu.style.animation = 'active 0.2s ease-in';
+		img.setAttribute('src', 'public/exit.png');
 		menu.style.display = 'flex';
 		menu.style.height = '100%';
 	} else {
 		menu.style.animation = 'remove 0.2s ease-in';
+		img.setAttribute('src', 'public/menu.png');
 		menu.style.height = '0px';
+
 		setTimeout(() => menu.style.display = 'none', 200);
 	}
+}
+
+
+
+const fnListClick = (e) => {
+	document.querySelector('#menu-wrap .now').classList.remove('now');
+	e.target.classList.add('now');
 }
