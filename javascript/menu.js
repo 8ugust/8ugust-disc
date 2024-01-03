a0b2b7976d0dcc525451f7cb1a206ff536129ec7
shmin <gks831@naver.com>
2024-01-03 22:36:38 +0900

    Update Menu CSS

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,4 +1,12 @@
 const fnMenuClick = () => {
 	const menu = document.getElementById('menu-wrap');
-	menu.style.display = (menu.style.display == 'none') ? 'flex' : 'none';
+	if (menu.style.display == 'none') {
+		menu.style.animation = 'active 0.2s ease-in';
+		menu.style.display = 'flex';
+		menu.style.height = '100%';
+	} else {
+		menu.style.animation = 'remove 0.2s ease-in';
+		menu.style.height = '0px';
+		setTimeout(() => menu.style.display = 'none', 200);
+	}
 }
