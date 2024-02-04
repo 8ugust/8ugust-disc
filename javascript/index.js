bfa82d092a3e931de693528b1964f8c57c8796b5
seonghan <gks831@naver.com>
2024-02-04 15:11:10 +0900

    Set WIndow Size Over 1000px

--- javascript/index.js
+++ javascript/index.js
@@ -18,15 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
 // ==================== ==================== ==================== ==================== ====================
 // ==================== ==================== ==================== ==================== ====================
 const fnSetSvgWidth = () => {
-	const per_30 = Math.round(Number(window.innerWidth) * 0.35);
-	const per_60 = Math.round(Number(window.innerWidth) * 0.7);
+	const root = document.getElementById('root');
+	const rWidth = Number(root.scrollWidth);
+
+	const per_30 = Math.round(rWidth * 0.35);
+	const per_60 = Math.round(rWidth * 0.7);
 
 	let dPath = "";
 	dPath += "M0 0 L0 60 ";
 	dPath += "L" + per_30 + " 150 ";
 	dPath += "L" + per_60 + " 20 ";
-	dPath += "L" + window.innerWidth + " 100 ";
-	dPath += "L" + window.innerWidth + " 0 ";
+	dPath += "L" + rWidth + " 100 ";
+	dPath += "L" + rWidth + " 0 ";
 
 	// M0 0 L0 60 L350 150 L700 20 L980 100 L980 0
 	const svg_desc = document.getElementById('svg-desc');
