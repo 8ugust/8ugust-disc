2ff869d3df3f1927697e646889b08dbb8bc46701
seonghan <gks831@naver.com>
2024-11-10 17:55:09 +0900

    index 스크립트 변경

--- javascript/index.js
+++ javascript/index.js
@@ -2,11 +2,12 @@
 // ==================== ==================== ==================== ==================== ==================== ====================
 document.addEventListener("DOMContentLoaded", () => {
 
-	fixAdStyle();
-	fnSetQuest();
-	fnSetSvgWidth();
-	addClass2Question();
-
+	fnSetSvgWidth();		/* SVG 생성 */
+	fnSetQuest();			/* 질문생성 */
+	
+	// 광고 사이즈에 따라서 광고 영역 변경하는 함수였는데 미사용 처리.
+	// 쿠팡 광고를 클릭형으로 볼 수 있는지 확인해보자.
+	// fixAdStyle();
 
 	// Test
 	// document.getElementById('test-wrap').style.display = 'none';
@@ -15,49 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-// ==================== ==================== ==================== ==================== ====================
-// ==================== ==================== ==================== ==================== ====================
-const fnSetSvgWidth = () => {
-	const root = document.getElementById('root');
-	const rWidth = Number(root.scrollWidth);
-
-	const per_30 = Math.round(rWidth * 0.35);
-	const per_60 = Math.round(rWidth * 0.7);
-
-	let dPath = "";
-	dPath += "M0 0 L0 60 ";
-	dPath += "L" + per_30 + " 150 ";
-	dPath += "L" + per_60 + " 20 ";
-	dPath += "L" + rWidth + " 100 ";
-	dPath += "L" + rWidth + " 0 ";
-
-	// M0 0 L0 60 L350 150 L700 20 L980 100 L980 0
-	const svg_desc = document.getElementById('svg-desc');
-	const exp_desc = document.getElementById('exp-desc');
-	svg_desc.setAttribute("d", dPath);
-	exp_desc.setAttribute("d", dPath);
-}
-
 
 
 // ==================== ==================== ==================== ==================== ====================
-// ==================== ==================== ==================== ==================== ====================
-const addClass2Question = () => {
-	Array.from(document.querySelectorAll('.question:not(.example)')).forEach((question, idx) => {
-		question.classList.add('quest_' + idx);
-	});
-}
-
-
-
-// ==================== ==================== ==================== ==================== ====================
+// ■ 이름 : fnSetQuest
+// ■ 내용 : 28개의 질문 영역을 생성.
 // ==================== ==================== ==================== ==================== ====================
 const fnSetQuest = () => {
 	const wrap = document.getElementById('quest_wrap');
-	this.global.questions.forEach(quest => {
+	this.global.questions.forEach((quest, idx) => {
 		let html = '';
 
-		html += '<div class="question">';
+		html += '<div class="question quest_ ' + (idx + 1) + '">';
 		html += 	'<div class="q-row">';
 		html += 		'<div class="q-cell q-head"><b>적합</b></div>';
 		html += 		'<div class="q-cell q-mid"></div>';
@@ -89,6 +59,35 @@ const fnSetQuest = () => {
 		wrap.appendChild(html.body.childNodes[0]);
 	})
 }
+
+
+
+
+
+// ==================== ==================== ==================== ==================== ====================
+// ==================== ==================== ==================== ==================== ====================
+const fnSetSvgWidth = () => {
+	const root = document.getElementById('root');
+	const rWidth = Number(root.scrollWidth);
+
+	const per_30 = Math.round(rWidth * 0.35);
+	const per_60 = Math.round(rWidth * 0.7);
+
+	let dPath = "";
+	dPath += "M0 0 L0 60 ";
+	dPath += "L" + per_30 + " 150 ";
+	dPath += "L" + per_60 + " 20 ";
+	dPath += "L" + rWidth + " 100 ";
+	dPath += "L" + rWidth + " 0 ";
+
+	// M0 0 L0 60 L350 150 L700 20 L980 100 L980 0
+	const svg_desc = document.getElementById('svg-desc');
+	const exp_desc = document.getElementById('exp-desc');
+	svg_desc.setAttribute("d", dPath);
+	exp_desc.setAttribute("d", dPath);
+}
+
+
 
 
 
