13ef0da5845be46b7cdb77fa9aa939b4be6ce528
shmin <gks831@naver.com>
2025-01-29 13:04:37 +0900

    MBTI 겹친다 개빡치네

--- javascript/question.js
+++ javascript/question.js
@@ -144,3 +144,48 @@ const fnQuestEnd = (test) => {
 	// 결과 설정
 	fnSetResult();
 }
+
+
+
+
+
+const fnSetResult = () => {
+
+	const result = this.global.result[1]; // 결과(DI)
+	const type = this.global.category[result][1]; // 대표유형(D)
+	const strength = this.global.type[type][1]; // 대표유형명
+	const color = this.global.type[type][0]; // 대표유형색
+
+	// 헤더에 결과값 설정
+	document.getElementById('type').innerHTML = this.global.category[result][0];
+	document.getElementById('strength').innerHTML = strength;
+	document.getElementById('disc').style.color = color;
+	document.getElementById('disc').innerHTML = result;
+
+	// 결과 분류 유형(D/I/S/C)에 따른 SVG 색상 설정
+	document.getElementById('svg-ratio-top').setAttribute('fill', color);
+	document.getElementById('svg-ratio-bot').setAttribute('fill', color);
+	document.getElementById('svg-wise-top').setAttribute('fill', color);
+	document.getElementById('svg-wise-bot').setAttribute('fill', color);
+	document.getElementById('type-ratio-main').style.background = color;
+	document.getElementById('summary').style.borderColor = color;
+	document.getElementById('wiseWrap').style.background = color;
+
+	// 각 유형 별 점수를 그래프로 환산하여 출력.
+	const scores = { D: 27, I: 28, S: 26, C: 24 };
+	this.global.result[0].forEach(([key, val]) => {
+		const percent = Math.floor(val/scores[key]*100);
+		document.getElementById('fill_' + key).style.width = percent + '%';
+		document.getElementById('num_' + key).innerHTML = percent + '%';
+	});
+
+	// 각 영역 설정
+	document.querySelector('#summary p').innerHTML = this.global.type[type][2];	// 요약
+	Array.from(document.getElementsByClassName('p-section')).forEach((p, idx) => {
+		p.innerHTML = this.global.category[result][Number(idx) + 2];
+	})
+
+	// 결과 화면 오픈
+	document.getElementById('test-wrap').style.display = 'none';
+	document.getElementById('result-wrap').style.display = 'flex';
+}
